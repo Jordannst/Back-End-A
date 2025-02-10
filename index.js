e4b695e7170ae10b6e54818c32030d9f4bbbe631
Jordannst <s22310272@student.unklab.ac.id>
2025-02-10 09:53:00 +0800

    practicing method & routing with express

--- index.js
+++ index.js
@@ -1,47 +1,31 @@
-const http = require("http");
+const express = require("express");
+const app = express();
+const port = 3003;
 const { hello, greetings } = require("./helloWorld");
 const moment = require("moment");
 
-const server = http.createServer((req, res) => {
-  // res.write(hello)
-  // res.write(greetings())
-  const url = req.url;
-  if (url === "/users") {
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "text/json");
-    res.write(
-      JSON.stringify({
-        status: "success",
-        message: "Users",
-        date: moment().format("MMMM Do YYYY, h:mm:ss a"),
-      })
-    );
-  } else if (url === "/post") {
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "text/json");
-    res.write(
-      JSON.stringify({
-        status: "success",
-        message: "Post",
-        date: moment().format("MMMM Do YYYY, h:mm:ss a"),
-      })
-    );
-  } else {
-    res.statusCode = 404;
-    res.setHeader("Content-Type", "text/json");
-    res.write(
-      JSON.stringify({
-        status: "not found",
-        message: "Route tidak ditemukan",
-        date: moment().format("MMMM Do YYYY, h:mm:ss a"),
-      })
-    );
-  }
-  res.end();
-});
+app.get("/", (req, res) => res.send("Hello World"));
+
+app.get("/about", (req, res) =>
+  res.status(200).json({
+    status: "success",
+    message: "about page",
+    data: [],
+  })
+);
+
+app.post("/content", (req, res) => res.send("request dengan method POST"));
+app.put("/contoh", (req, res) => res.send("request dengan method PUT"));
+app.delete("/contoh", (req, res) => res.send("request dengan method DELETE"));
+app.patch("/contohpatch", (req, res) =>
+  res.send("request dengan method PATCH")
+);
+app.all("/universal", (req, res) => res.send(`Request Method ${req.method}`)); // Method universal/ Semua Method
+
+// Routing dinamis menggunakan Params
+app.get("/post/:id", (req, res) => res.send(`Artikel ke - ${req.params.id}`));
 
 const hostname = "127.0.0.1";
-const port = 3001;
-server.listen(port, hostname, () =>
+app.listen(port, hostname, () =>
   console.log(`Server running at http://${hostname}:${port}`)
 );
