e3a646a850faf4a19b0fbfd5d46d947f314b97c3
Jordannst <s22310272@student.unklab.ac.id>
2025-03-05 08:49:08 +0800

    add upload router

--- routers.js
+++ routers.js
@@ -1,52 +1,72 @@
 const express = require("express");
 const routers = express.Router();
 const path = require("path");
-// ROuting
-routers.get("/download", (req, res) => {
-  const filename = "dummy.png";
-  // res.download(path.join(__dirname, filename), filename);
-  res.download(path.join(__dirname, "/download", filename));
+const fs = require("fs");
+const multer = require("multer");
+
+const imageFilter = (req, file, cb) => {
+  if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/)) {
+    return cb(null, false);
+  }
+  cb(null, true);
+};
+
+const upload = multer({ dest: "public", fileFilter: imageFilter });
+
+// Routing
+routers.post("/upload", upload.single("file"), (req, res) => {
+  const file = req.file;
+  console.log(file);
+  if (file) {
+    const target = path.join(__dirname, "public", file.originalname);
+    fs.renameSync(file.path, target); //rename file agar sama dengan original file name
+    res.send("file berhasil diupload");
+  } else {
+    res.send("file gagal diupload");
+  }
 });
 
-routers.get("/", (req, res) => res.send("Hello World"));
-routers.get("/about", (req, res) =>
-  res.status(200).json({
-    status: "success",
-    message: "about page",
-    data: [],
-  })
-);
+routers.get("/download", (req, res) => {
+  const filename = "dummy.png";
+  res.download(path.join(__dirname, "/download", filename), "dummy-photo.png");
+});
 
 routers.post("/login", (req, res) => {
   const { username, password } = req.body;
   res.status(200).json({
     status: "success",
-    message: "Login Berhasil",
-    data: { username: username, password: password },
+    message: "Login page",
+    data: {
+      username: username,
+      password: password,
+    },
   });
 });
+routers.get("/", (req, res) => res.send("Hello World"));
+routers.get("/about", (req, res) =>
+  res.status(200).json({
+    status: "success",
+    message: "About page",
+    data: [],
+  })
+);
+routers.post("/contoh", (req, res) => res.send("request method POST"));
+routers.put("/contoh", (req, res) => res.send("Request method PUT"));
+routers.delete("/contoh", (req, res) => res.send("Request method DELETE"));
+routers.patch("/contoh", (req, res) => res.send("Request method PATCH"));
 
-routers.post("/content", (req, res) => res.send("request dengan method POST"));
-routers.put("/contoh", (req, res) => res.send("request dengan method PUT"));
-routers.delete("/contoh", (req, res) =>
-  res.send("request dengan method DELETE")
-);
-routers.patch("/contohpatch", (req, res) =>
-  res.send("request dengan method PATCH")
-);
 routers.all("/universal", (req, res) =>
-  res.send(`Request Method ${req.method}`)
-); // Method universal/ Semua Method
-
-// 1. Routing dinamis menggunakan Params
+  res.send(`Request method ${req.method}`)
+);
+// Routing dinamis
+// 1. Menggunakan params
 routers.get("/post/:id", (req, res) =>
   res.send(`Artikel ke - ${req.params.id}`)
 );
-
-// 2. Routing dinamis menggunakan Query String
+// 2. Menggunakan Query String
 routers.get("/post", (req, res) => {
   const { page, sort } = req.query;
-  res.send(`Query yang didapatkan adalah:  ${page}, sort: ${sort}`);
+  res.send(`Query string= page :${page}, sort : ${sort}`);
 });
 
 module.exports = routers;
